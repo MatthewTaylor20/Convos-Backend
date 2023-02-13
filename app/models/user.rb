c2ebba2e8409ee8aae74765fa6353c9f348f6015
Matthew Taylor <mjt.inbox@gmail.com>
2023-02-13 13:00:55 -0700

    added relationships to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  has_many :messages
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
